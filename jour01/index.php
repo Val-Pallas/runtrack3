1d70d894e156eaff3481c395a65312fa5c089338
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-09-05 11:37:30 +0200

    creation de pop up java script

---
+++ jour01/index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pop Up!</title>
+    <script>
+        // JavaScript pour afficher une popup
+        window.onload = function() {
+            alert("Hello JavaScript!");
+        };
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
