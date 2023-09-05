cda5abb03735fd60945138ff3b7486638208eb15
Abundancia <abundancia@MacBook-Air-de-Abundancia.local>
2023-09-05 12:26:43 +0200

    fonction javascript

---
+++ jour01/job05/index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afficher les jours de la semaine</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1>Liste des jours de la semaine</h1>
+    <script>
+        afficherjourssemaines(); 
+    </script>
+    
+</body>
+</html>
+
---
+++ jour01/job05/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+function afficherjourssemaines() {
+    const jourssemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for (let x in jourssemaines) {
+        console.log(jourssemaines[x]);        
+    }
+}
